95871ba8ad91aa1dfdaadd3631113de37ce60ddc
Gavin-Arneson <82293960+ttgannon@users.noreply.github.com>
2023-06-09 11:22:31 -0400

    arrows

---
+++ arrows/arrows.js
@@ -0,0 +1,24 @@
+function double(arr) {
+    return arr.map(function(val) {
+      return val * 2;
+    });
+  }
+
+  //refactor: 
+
+const double = arr => arr.map(val => val*2);
+  
+
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+  }
+
+  //refactor:
+
+  const squareAndFindEvens = numbers => numbers.map(num => num **2).filter(square => square % 2 === 0)
