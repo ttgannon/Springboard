cd5c94ba0bd8b853de187f4732249a5b7053e81d
Gavin-Arneson <82293960+ttgannon@users.noreply.github.com>
2023-06-22 11:36:08 -0400

    big o notation

---
+++ big-o/big-o.js
@@ -0,0 +1,51 @@
+1. O(n + 10)
+n
+
+2. O(100 * n)
+n
+
+3. O(25)
+1
+
+4. O(n^2 + n^3)
+n^3
+
+5. O(n + n + n + n)
+n
+
+6. O(1000 * log(n) + n)
+n
+
+7. O(1000 * n * log(n) + n)
+nlog(n)
+
+8. O(2^n + n^2)
+2^n
+
+9. O(5 + 3 + 1)
+1
+
+10. O(n + n^(1/2) + n^2 + n * log(n)^10)
+n^2
+
+time complexity: N
+time complexity: N
+time complexity: 1
+time complexity: N
+time complexity: N^2
+time complexity: N
+
+1. True
+2. True
+3. False
+4. n
+5. n
+6. n
+7. Nlog(n)
+8. N
+9. 1
+10. n
+11. 1
+12. N
+13. N
+
